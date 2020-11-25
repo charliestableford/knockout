af736fc1a61dac0699e025ac88e559e9bd469a2b
Charlie Stableford <charliestableford2@gmail.com>
2020-11-25 08:34:20 -0800

    this

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ console.log("in");
 
 $(function() {
    var myViewModel = function(myCourse, myRating){
+       // everytime you are using this it is referencing the current function
        this.course = ko.observable(myCourse);
        this.rating = ko.observable(myRating);
+       // this new funciton has its own this. it is referencing the funciton it is in not the one above. 
+       this.courseResult = ko.computed(function(){
+            return "The course" + this.course() + "got a rating of" + this.rating();
+       }, )
    }
 });
